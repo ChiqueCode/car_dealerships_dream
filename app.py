c33d7a48d21f882e9d03b11870601829af0940b1
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-06-08 13:48:47 -0400

    🚧added database

--- app.py
+++ app.py
@@ -15,22 +15,37 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
-db = SQLAlchemy(app)
+rds_connection_string = f"root:121212nJ@127.0.0.1/leads_db"
+engine = create_engine(f'mysql+pymysql://{rds_connection_string}')
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
+leads_df = pd.read_sql('SELECT * FROM leadscsv', con=engine)
+print(leads_df.head())
+# app.config["SQLALCHEMY_DATABASE_URI"] = ""
+# db = SQLAlchemy(app)
 
-# Save references to each table
-Samples_Metadata = Base.classes.sample_metadata
-Samples = Base.classes.samples
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(db.engine, reflect=True)
+
+# # Save references to each table
+# TENNISDATA = Base.classes.TENNISDATA
 
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
+
+@app.route("/data")
+def get_data():
+    """Return a list of sample names."""
+
+    # Use Pandas to perform the sql query
+    stmt = db.session.query(leads_df).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    # Return a list of the column names (sample names)
+    return jsonify(list(df.columns)[2:])    
 
 
 
---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+function buildMetadata(sample) {
+    var url = "/names";
+  
+    //Use `d3.json` to fetch the metadata for a sample
+    d3.json(url).then(function(data) {
+      console.log(data);
+  
+      });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
