1f4d0147c6e18e727d2b9785a4fdd5ed3c3e5fe3
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-06-11 13:22:49 -0400

    🚧Created "/data" route -> key, value pair

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(db.engine, reflect=True)
 
-# Save references to each table
+# Creating an easier reference
 Leads_table = Base.classes.leads_table
 
 # Home route 
@@ -30,11 +30,38 @@ def index():
     return render_template("index.html")
 
 # Route for testing the data 
-@app.route("/test")
-def test_func():
-    stmt = db.session.query(Leads_table).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-    return jsonify(list(df))
+# @app.route("/test")
+# def test_func():
+#     stmt = db.session.query(Leads_table).statement
+#     df = pd.read_sql_query(stmt, db.session.bind)
+#     return jsonify(list(df["Zip"]))
+
+
+# Data route
+@app.route("/data")
+def get_data():
+
+    # All the columns for selection
+    sel = [
+        Leads_table.Zip,
+        Leads_table.City,
+        Leads_table.State,
+        Leads_table.Gender,
+        Leads_table.Age,
+        Leads_table.MaritalStatus,
+        Leads_table.CreditScore,
+        Leads_table.Kids,
+        Leads_table.Email_Address
+    ]
+
+    # Saving all the data from quering above columns
+    results = db.session.query(*sel).all()
+
+    # Creating Pandas dataframe
+    df = pd.DataFrame(results, columns=["Zip", "City", "State", "Gender", "Age", "MaritalStatus", "CreditScore", "Kids", "Email_Address"])
+
+    # Return results in JSON format for the interwebz
+    return jsonify(df.to_dict(orient="records"))
 
 
 if __name__ == "__main__":
