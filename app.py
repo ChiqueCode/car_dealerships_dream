baecd37a586ea7f49f795fe79df0983542c38603
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-06-12 19:07:33 -0400

    🚧Created a new route for data filtered by nj, created a table with fiilters
    ! Created a new route for data filtered by nj as json can't return our whole data

--- app.py
+++ app.py
@@ -58,15 +58,46 @@ def get_data():
     ]
 
     # Saving all the data from quering above columns
-    # results = db.session.query(*sel).\limit(10).all()
-    results = db.session.query(*sel).\
-        limit(500).all()
+    # results = db.session.query(*sel).\
+    #     limit(500).all()
+
+    # Query all the records
+    results = db.session.query(*sel).all()         
 
     # Creating Pandas dataframe
     df = pd.DataFrame(results, columns=["ConsumerID", "Zip", "Audience_Count", "City", "State", "Gender", "Age", "MaritalStatus", "EthnicGroup", "CreditScore", "Kids", "Email_Address"])
 
     # Return results in JSON format for the interwebz
     return jsonify(df.to_dict(orient="records"))
+
+@app.route("/data/nj")
+def get_data_by_state():
+
+    # All the columns for selection
+    sel = [
+        Leads_table.ConsumerID,
+        Leads_table.Zip,
+        Leads_table.Audience_Count,
+        Leads_table.City,
+        Leads_table.State,
+        Leads_table.Gender,
+        Leads_table.Age,
+        Leads_table.MaritalStatus,
+        Leads_table.EthnicGroup,
+        Leads_table.CreditScore,
+        Leads_table.Kids,
+        Leads_table.Email_Address
+    ]
+
+    results_by_state = db.session.query(*sel).\
+        filter(Leads_table.State == "NJ").\
+        all()           
+
+    # Creating Pandas dataframe
+    df_by_state = pd.DataFrame(results_by_state, columns=["ConsumerID", "Zip", "Audience_Count", "City", "State", "Gender", "Age", "MaritalStatus", "EthnicGroup", "CreditScore", "Kids", "Email_Address"])
+
+    # Return results in JSON format for the interwebz
+    return jsonify(df_by_state.to_dict(orient="records"))
 
 
 if __name__ == "__main__":
